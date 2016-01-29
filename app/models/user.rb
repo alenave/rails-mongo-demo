9cd462bdfd7a16e2ac1b3328bc19ac2f2a73115e
Ale <ale.nave@scripbox.com>
2016-01-30 01:09:48 +0530

    validation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User
   field :email_id, type: String
   field :mobile_number, type: Integer
   field :loan_amount, type: Float
-  field :tenure, type: Float
+  field :tenure, type: Integer
+  validates :loan_amount, :presence => true, :numericality => true
+  validates :mobile_number, :presence => true, :numericality => true, length: { minimum: 10, maximum: 10 }
+  validates :tenure, :presence => true, :numericality => true, length: { minimum: 1, maximum: 2 }
 end
