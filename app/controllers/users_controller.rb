1a50c3e3c4dcb0d64a9db0e7f62cda96e539621d
Ale <ale.nave@scripbox.com>
2016-01-29 22:16:51 +0530

    Ajax complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,13 +27,15 @@ class UsersController < ApplicationController
   end
 
 
-  def calculation(loan_amount = params[:loan_amount].to_i, tenure = 10)
+  def calculation(loan_amount = params[:loan_amount].to_i, tenure = params[:tenure].to_i)
     display_details = []
     hash = {}
     bank_details = BankDetail.where(:loan_min_vl.lte => loan_amount, :loan_max_vl.gte => loan_amount, :loan_tenure.gte => tenure)
     bank_details.each do |bank_detail|
       bank_logo = BankLogo.where(bank_name: bank_detail.bank_name).first.logo_base64
       emi = Amortization.calculate(loan_amount, bank_detail.rate, tenure)
+   
+
       hash.merge!({"bank_logo" => bank_logo, "interest_rate" => bank_detail.rate, "processing_amount" => bank_detail.processing_amount, "emi" => emi})
       display_details.push(hash.clone)
       hash = {}
